bb03cbc6561d2ec360d2a90c53a61268bfcc36f0
Théo SIKLI <theo@theo-sikli.fr>
2020-12-06 00:23:22 +0100

    Get all customers & return them

--- controllers/customers.controller.ts
+++ controllers/customers.controller.ts
@@ -2,11 +2,19 @@ import {Request, Response} from "express";
 import {Customer} from "../models";
 
 export class CustomersController {
-    public async addNewCustomer(req: Request, res: Response) {
+    public addNewCustomer(req: Request, res: Response) {
         Customer.create(req.body).then(customer => {
             res.status(201).json(customer)
         }).catch(err => {
             res.status(500).json(err);
         });
+    }
+
+    public getAllCustomers(req: Request, res: Response) {
+        Customer.findAll().then(customers => {
+            res.status(200).json(customers);
+        }).catch(err => {
+            res.status(500).json(err);
+        })
     }
 }
